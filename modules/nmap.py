710142cdafa0aa1bf2d7e003f12a1318e273163e
jatin <jatink07624@gmail.com>
2024-06-17 22:50:35 +0530

    fixing modules

--- modules/nmap.py
+++ modules/nmap.py
@@ -2,7 +2,7 @@ import subprocess
 
 def run_nmap(domain):
     try:
-        result = subprocess.run(['nmap', '-A', domain], capture_output=True, text=True)
+        result = subprocess.run(['nmap', '--traceroute', domain], capture_output=True, text=True)
         return result.stdout
     except Exception as e:
         print(f"Error running Nmap scan for {domain}: {e}")
